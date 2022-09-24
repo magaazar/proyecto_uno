a210ea334483c7779674ded4d17c4cdb0f41118a
Gabriela Azar <gabriela.azar@ort.edu.ar>
2022-09-24 18:56:54 -0300

    se actualizan views

--- cac/views.py
+++ cac/views.py
@@ -6,3 +6,14 @@ from django.shortcuts import render
 
 def hola_mundo(request):
     return HttpResponse("Hola mundo Django")
+
+def saludar(request,nombre="Pepe"):
+     return HttpResponse(f"""
+        <h1>Hola Mundo Django - {nombre}</h1>
+    """)	
+
+def ver_proyectos(request,anio,mes):
+    return HttpResponse(f"""
+        <h1>Proyectos del  - {mes}/{anio}</h1>
+        <p>Listado de proyectos</p>
+    """)
